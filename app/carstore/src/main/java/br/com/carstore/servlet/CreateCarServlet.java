0c59a0a06f5782c323d25fbf6a0e88daf47a0bc1
YAGO DE AMORIM DOS SANTOS <yago.asantos3@senacsp.edu.br>
2024-09-27 20:23:49 -0300

    Criando o método para INSERT e configurando a conexão com o com o banco de dados

--- app/carstore/src/main/java/br/com/carstore/servlet/CreateCarServlet.java
+++ app/carstore/src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -1,5 +1,8 @@
 package br.com.carstore.servlet;
 
+import br.com.carstore.dao.CarDao;
+import br.com.carstore.model.Car;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,9 +16,19 @@ public class CreateCarServlet extends HttpServlet {
 
     @java.lang.Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String carName = req.getParameter("car-name");
 
-        System.out.println(carName);
+        String carName = req.getParameter("name");
+        String carColor = req.getParameter("color");
+
+        Car car = new Car();
+        car.setName(carName);
+        car.setColor(carColor);
+
+        CarDao carDao = new CarDao();
+        carDao.createCar(car);
+
+        //System.out.println(carName);
+        //System.out.println(carColor);
 
         req.getRequestDispatcher("index.html").forward(req, resp);
 
